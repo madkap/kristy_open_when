f00ecdb8abc8869328d0bdd3b897c1af03dbb334
Michael Dauz <michael.dauz@workday.com>
2017-07-14 18:04:39 -0700

    root link to splash page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :links
-  root to: 'links#index'
+  root to: 'open_when#splash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
