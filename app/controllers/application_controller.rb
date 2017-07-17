618feaf1cfe0ddc9d08433ba98ad6098073c7d27
Michael Dauz <michael.dauz@workday.com>
2017-07-17 13:23:20 -0700

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with(
+    name: ENV.fetch('BASIC_AUTH_USERNAME'),
+    password: ENV.fetch('BASIC_AUTH_PASSWORD')
+  )
+
 end
